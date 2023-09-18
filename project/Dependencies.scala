88d08411211603c58b9489f817626b2eddd17906
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-09-18 12:57:30 +0000

    Update circe-parser to 0.14.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,8 +8,8 @@ object Dependencies {
   lazy val lambdaEvents = "com.amazonaws" % "aws-lambda-java-events" % "3.11.1"
   lazy val preservicaClient = "uk.gov.nationalarchives" %% "preservica-client-fs2" % "0.0.5"
   lazy val s3Client = "uk.gov.nationalarchives" %% "da-s3-client" % "0.1.3"
-  lazy val circeParser = "io.circe" %% "circe-parser" % "0.14.5"
+  lazy val circeParser = "io.circe" %% "circe-parser" % "0.14.6"
   lazy val scalaXml = "org.scala-lang.modules" %% "scala-xml" % "2.1.0"
   lazy val scalaParserCombinators = "org.scala-lang.modules" %% "scala-parser-combinators" % "2.2.0"
-  lazy val jaxb= "javax.xml.bind" % "jaxb-api" % "2.3.1"
+  lazy val jaxb = "javax.xml.bind" % "jaxb-api" % "2.3.1"
 }
