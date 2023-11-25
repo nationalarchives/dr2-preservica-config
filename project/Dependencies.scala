2187b562748d2b4c132439b959b01443484cabb5
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-11-25 08:24:17 +0000

    Update preservica-client-fs2 to 0.0.28

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   lazy val pureConfig = "com.github.pureconfig" %% "pureconfig" % "0.17.4"
   lazy val lambdaCore = "com.amazonaws" % "aws-lambda-java-core" % "1.2.3"
   lazy val lambdaEvents = "com.amazonaws" % "aws-lambda-java-events" % "3.11.3"
-  lazy val preservicaClient = "uk.gov.nationalarchives" %% "preservica-client-fs2" % "0.0.21"
+  lazy val preservicaClient = "uk.gov.nationalarchives" %% "preservica-client-fs2" % "0.0.28"
   lazy val s3Client = "uk.gov.nationalarchives" %% "da-s3-client" % "0.1.23"
   lazy val circeParser = "io.circe" %% "circe-parser" % "0.14.6"
   lazy val scalaXml = "org.scala-lang.modules" %% "scala-xml" % "2.2.0"
